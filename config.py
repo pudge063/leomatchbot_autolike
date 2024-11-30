f2e3aa3db6a4739b15cdef4208a1eef5881af853
Арсений Никулин <pudge063@Arsenijs-MacBook-Air.local>
2024-11-30 23:49:52 +0400

    pipeline

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-api_id = os.getenv("api_id")
-api_hash = os.getenv("api_hash")
-phone_number = os.getenv("phone_number")
+api_id = os.getenv("API_ID")
+api_hash = os.getenv("API_HASH")
+# phone_number = os.getenv("phone_number")
 
-SAVE_PATH = "all_images"
+# SAVE_PATH = "all_images"
